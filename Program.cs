97dd5aa35c31fb7436c5a6919250cc16b2c780ca
Pavel Svec <pavel.svec@itcrawl.cz>
2019-11-20 09:56:45 +0100

    Task

--- Program.cs
+++ Program.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AsyncModels
 {
     class Program
     {
+        public async void Print()
+        {
+            Task t;
+        }
         static void Main(string[] args)
         {
         }
