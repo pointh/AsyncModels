e6dabd7244c877da5a882996e649ea43e45e425c
Pavel Svec <pavel.svec@itcrawl.cz>
2019-11-21 16:33:47 +0100

    Threadpool APM

--- Program.cs
+++ Program.cs
@@ -1,20 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+using System.Threading;
 
-namespace AsyncModels
+
+namespace ThreadpoolAPM
 {
+    /// <summary>
+    /// Threadpool async programming model
+    /// </summary>
     class Program
     {
-        public async void Print()
+        // Delegát WaitCallback je definovaný s argumentem typu object
+        public static void Print(object s)
         {
-            Task t;
+            Console.WriteLine(s);
         }
         static void Main(string[] args)
         {
+            WaitCallback wc = Print;
+
+            ThreadPool.GetMaxThreads(out int nThreads, out int nCompletionPortThreads);
+            Console.WriteLine($"Začátek: Maximálně {nThreads} vláken ve threadpoolu, maximálně {nCompletionPortThreads} I/O vláken v threadpoolu");
+
+            // Skončí v náhodném pořadí
+            ThreadPool.QueueUserWorkItem(wc, "1");
+            ThreadPool.QueueUserWorkItem(wc, "2");
+            ThreadPool.QueueUserWorkItem(wc, "3");
+            ThreadPool.QueueUserWorkItem(wc, "4");
+            ThreadPool.QueueUserWorkItem(wc, "5");
+
+            Console.ReadKey();
         }
     }
 }
